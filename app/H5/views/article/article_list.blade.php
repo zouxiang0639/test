79e6cc55787e3f6c9aef971c1241ac3ddf02eed6
Zou Xiang <zouxiang0639@icloud.com>
2018-12-07 13:24:53 +0800

    update

--- app/H5/views/article/article_list.blade.php
+++ app/H5/views/article/article_list.blade.php
@@ -4,15 +4,19 @@
             <li>
                 <div class="tit clearfix">
                     <a href="{!! route('h.article.info', ['id'=> $item->id]) !!}">
-                        <i style="color:{!!  Forum::Tags()->getTagsColor($item->tags) !!} " class="{!!  Forum::Tags()->getTagsIcon($item->tags) !!} icon"></i>
-                        <p>{{ $item->title }}<span class="num">[{!! $item->replyCount !!}]</span></p>
+                        <i style="color:{!!  Forum::Tags()->getTagsColor($item->tags) !!}; float: left " class="{!!  Forum::Tags()->getTagsIcon($item->tags) !!} icon"></i>
+                       <p style="float: left ;width: 90%">
+
+                            {{ $item->title }}
+                           <span class="num">[{!! $item->replyCount !!}]</span>
+                       </p>
                     </a>
                 </div>
                 <div class="des clearfix">
                     <p>
-                        <i class="no">{!! $item->id !!}</i>
-                        <i class="name">{!! $item->issuers ? $item->issuers->name : '-' !!}</i>
-                        <i class="date">{!! $item->created_at !!}</i>
+                        <span><i>{!! $item->id !!}</i></span>
+                        <span><i class="name">{!! $item->issuers ? $item->issuers->name : '-' !!}</i></span>
+                        <span><i class="date">{!! $item->created_at !!}</i></span>
                         <span class="read"><i class="ico"></i>{!! $item->browse !!}</span>
                         <span class="good"><i  class="fa fa-thumbs-o-up"></i>
                             <span >{{ $item->recommend_count }}</span>
--- app/H5/views/partials/header.blade.php
+++ app/H5/views/partials/header.blade.php
@@ -1,23 +1,30 @@
-<div class="header clearfix">
-    <div class="logo fl"><a href="{!! route('h.home') !!}"><img src="{{  assets_path("/h5/img/logo.png") }}" alt="" title="" /></a></div>
-    <div class="hd-link fr clearfix">
-        @if(Auth::guard('forum')->check())
-            <?php
-            $count = \App\Forum\Bls\Article\InfoBls::countInfo(\Auth::guard('forum')->id(), \App\Consts\Common\WhetherConst::NO);
-            ?>
-            <a  class="info-link" href="{!! route('h.member.info') !!}">
-                <i style="{!! $count ? 'color: #e15844;': 'color: #ffffff;' !!}" class="icon-alarm icon"></i>
-                <span style="{!! $count ? 'color: #e15844;': 'color: #ffffff;' !!}">{!! \App\Forum\Bls\Article\InfoBls::countInfo(\Auth::guard('forum')->id(), \App\Consts\Common\WhetherConst::NO) !!}</span>
-            </a>
-            <a class="my-link" href="{!! route('h.member.index') !!}"></a>
-        @else
-            <a class="my-link" href="{!! route('h.auth.login') !!}"></a>
-        @endif
+<div style="box-shadow: 0 0 0.2rem #061323">
+    <div class="header clearfix">
+        <div class="logo fl"><a href="{!! route('h.home') !!}"><img src="{{  assets_path("/h5/img/logo.png") }}" alt="" title="" /></a></div>
+        <div class="hd-link fr clearfix">
+            @if(Auth::guard('forum')->check())
+                <?php
+                $count = \App\Forum\Bls\Article\InfoBls::countInfo(\Auth::guard('forum')->id(), \App\Consts\Common\WhetherConst::NO);
+                ?>
+                <a  class="info-link" href="{!! route('h.member.info') !!}">
+                    <i style="{!! $count ? 'color: #e15844;': 'color: #ffffff;' !!}" class="icon-alarm icon"></i>
+                    <span style="{!! $count ? 'color: #e15844;': 'color: #ffffff;' !!}">{!! \App\Forum\Bls\Article\InfoBls::countInfo(\Auth::guard('forum')->id(), \App\Consts\Common\WhetherConst::NO) !!}</span>
+                </a>
+                <a class="my-link on" href="{!! route('h.member.index') !!}">
+                    {{--<i class="fa fa-user"></i>--}}
+                </a>
+            @else
+                <a class="my-link" href="{!! route('h.auth.login') !!}">
+                    {{--<i class="fa fa-user"></i>--}}
+                </a>
+            @endif
 
-        <a class="edit-link" href="{!! route('h.article.create') !!}"></a>
-        <a class="search-link" href="{!! route('h.article.search') !!}"></a>
+            <a class="edit-link" href="{!! route('h.article.create') !!}"></a>
+            <a class="search-link" href="{!! route('h.article.search') !!}"></a>
+        </div>
     </div>
 </div>
+
 <div class="menu-box">
     <a @if(Input::get('type') == 'hot') class="on" @endif href="{!! route('h.home') !!}">热门</a>
     <a @if(Input::get('type') == 'new') class="on" @endif href="{!! route('h.home', ['type' => 'new']) !!}">最新</a>
